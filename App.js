de866c44047fc40b36d2fa946beed21160625e81
Shantanu Tiwari <127835064+shantanutewari12@users.noreply.github.com>
2023-07-29 02:42:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import ExpenseItems from './components/ExpenseItems';
+function App() {
+  return (
+    <div>
+   <h1>Expense tracker</h1>
+  <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
