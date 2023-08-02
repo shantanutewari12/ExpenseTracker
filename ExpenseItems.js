57aebf5a83fddc532f8f285e21c4bc142d0b348b
Shantanu Tiwari <127835064+shantanutewari12@users.noreply.github.com>
2023-08-02 16:16:07 +0530

    Add files via upload
    I hve added a new change title button in it

--- ExpenseItems.js
+++ ExpenseItems.js
@@ -1,22 +1,22 @@
-import "./ExpenseItem.css"
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css'
 
-function ExpenseItems(props) {
-const month = props.date.toLocaleString('en-US', { month: 'long' });
-const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-const year = props.date.getFullYear();
+const ExpenseItems = (props) => {
+
+  const clickHandler = () => {
+    console.log('Clicked!')
+  }
 
     return (
-      <div className="expense-item">
-<div>
-<div>{month}</div>
-<div>{day}</div>
-<div>{year}</div>
-</div>
+      <Card className="expense-item">
+<ExpenseDate date={props.date} />
 <div className="expense-item__description">
   <h2>{props.title}</h2>
   <div className="expense-item__price">${props.amount}</div>
 </div>
-</div>
+<button onClick={clickHandler}>Change title</button>
+</Card>
     );
   }
   
