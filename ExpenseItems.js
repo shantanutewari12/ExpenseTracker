e6647e9e8bc3e37274eda7cacb2ca60c27d04278
Shantanu Tiwari <127835064+shantanutewari12@users.noreply.github.com>
2023-08-02 17:55:40 +0530

    Add files via upload
    Added a change amount button that changes the amount to 100$

--- ExpenseItems.js
+++ ExpenseItems.js
@@ -1,21 +1,33 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItems = (props) => {
 
+const [title,setTitle] =  useState(props.title)
+const [amount,setAmount] = useState(props.amount)
+
   const clickHandler = () => {
-    console.log('Clicked!')
+    setTitle('Updated!')
+    //console.log(title)
   }
+ 
+  const clickAmountHandler = () => {
+  setAmount(100)
+  }
+
 
     return (
       <Card className="expense-item">
 <ExpenseDate date={props.date} />
 <div className="expense-item__description">
-  <h2>{props.title}</h2>
-  <div className="expense-item__price">${props.amount}</div>
+  <h2>{title}</h2>
+  <div className="expense-item__price">${amount}</div>
 </div>
 <button onClick={clickHandler}>Change title</button>
+<button onClick={clickAmountHandler}>Change amount</button>
 </Card>
     );
   }
